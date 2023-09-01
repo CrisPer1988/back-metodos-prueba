752c78b9b633c0068203bde4133db0f9cbbfd68d
Mauricio <madegasperis@gmail.com>
2023-09-01 11:46:19 -0300

    slack thread

--- src/controller/slack.controller.js
+++ src/controller/slack.controller.js
@@ -47,6 +47,62 @@ exports.createChannel = async (req, res) => {
       }
 }
 
+exports.getTimestamp = async (req, res) => {
+  const {channelId, token} = req.body
+  const slack = new WebClient('xoxp-5834083025858-5857782466688-5822903092167-efbdf6e40198ef6c162f7abaa8601add')
+
+  try {
+    const response = await slack.conversations.history({
+      channel: channelId,
+      limit: 1
+    })
+
+    if (response.ok && response.messages.length > 0) {
+      const firstMessage = response.messages[0].ts
+      return res.status(200).json({
+        message: 'Timestamp del primer mensaje en el canal:', firstMessage
+      })
+      
+    } else {
+      return res.status(500).json({
+        message: 'No se pudo obtener el timestamp del mensaje.'
+      })
+    }
+  } catch (error) {
+      console.error('Error:', error)
+      return res.status(500).json ({
+        message: 'Error'
+      })
+  }
+}
+
+exports.createThread = async (req, res) => {
+  const {channelId, messageTimestamp, threadText, token} = req.body
+  const slack = new WebClient('xoxp-5834083025858-5857782466688-5822903092167-efbdf6e40198ef6c162f7abaa8601add')
+
+  try {
+    const response = await slack.chat.postMessage({
+      channel: channelId,
+      text: threadText,
+      thread_ts: messageTimestamp
+    })
+
+    if (response.ok) {
+      return res.status(200).json({
+        message: 'Hilo creado con exito'
+      })
+    } else {
+      return res.status(500).json({
+        message: 'No se pudo crear el hilo'
+      })
+    }
+  } catch (error) {
+    return res.status(500).json ({
+      message: 'Error:', error 
+    })
+  }
+}
+
 exports.addMemberToChannel = async (req, res) => {
   const { channelId, userId, token } = req.body;
   const slack = new WebClient(token);
@@ -116,7 +172,7 @@ exports.getUserId = async (req, res) => {
 /////////////////////////lista de canales
 
 exports.listChannel = async (req, res) => {
-  const slack = new WebClient("xoxb-5836473505844-5819589606151-2wDZZ0HBjTrHx1BS9WrzBlgv");
+  const slack = new WebClient("xoxp-5834083025858-5857782466688-5822903092167-efbdf6e40198ef6c162f7abaa8601add");
   try {
     const response = await slack.conversations.list();
     if (response.ok) {
--- src/router/slack.routes.js
+++ src/router/slack.routes.js
@@ -9,8 +9,12 @@ router.post("/send-message", slackController.sendMessage)
 
 router.post('/create', slackController.createChannel)
 
+router.post('/create-thread', slackController.createThread)
+
 router.get("/list-channel", slackController.listChannel)
 
 router.get("/myId", slackController.getUserId)
+
+router.get('/timestamp', slackController.getTimestamp)
 
 module.exports = router
